ed758793263f6b577a994c5dccb7f109e8b4b07e
Andranik Badalyan <andranikvbadalyan@gmail.com>
2018-08-21 10:19:02 -0700

    Bcrypt user table seeder password

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
         $user = new User();
         $user->name = 'Andranik Badalyan';
         $user->email = 'andranikvbadalyan@gmail.com';
-        $user->password =  '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm'; // secret
+        $user->password =  bcrypt('secret');
         $user->remember_token = str_random(10);
         $user->save();
 
